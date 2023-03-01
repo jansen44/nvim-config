d23771a567247f92a2dccc9ff379f444c5707221
Jansen Junior <jsd.alcantara@gmail.com>
2023-02-28 22:21:40 -0400

    fix tab

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -9,6 +9,12 @@ require("mason-lspconfig").setup {
 }
 
 local cmp = require'cmp'
+local luasnip = require("luasnip")
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
     snippet = {
@@ -39,6 +45,7 @@ cmp.setup({
             fallback()
           end
         end, { "i", "s" }),
+
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
