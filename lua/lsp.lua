c3a56d8e5b9507b6f98628f5bdb10b8d6f2bebd1
Jansen Junior <jsd.alcantara@gmail.com>
2023-03-06 20:06:11 -0400

    remap code actions

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -91,7 +91,7 @@ local on_attach = function(client, bufnr)
     end, bufopts)
     vim.keymap.set('n', 'gf', vim.lsp.buf.type_definition, bufopts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<leader>ka', vim.lsp.buf.code_action, bufopts)
     vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
     vim.keymap.set('n', '<leader>F', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
