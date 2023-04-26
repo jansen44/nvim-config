b81eeb1b46216943b58ede62cf2015b6d4b81fd5
Jansen Junior <jsd.alcantara@gmail.com>
2023-04-26 11:16:29 -0400

    removed unnecessary term keybinds

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -26,41 +26,8 @@ vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Previous()', { silent = true, exp
 
 -- Terminals
 local Terminal = require("toggleterm.terminal").Terminal
-local terms = {}
-
-local term_opts = {
-    { cmd = "zsh", direction = "horizontal", size = 0.3 },
-    { cmd = "zsh", direction = "float",      size = nil },
-}
-
-for i, term in pairs(term_opts) do
-    terms[i] = Terminal:new({ cmd = "zsh", count = i, direction = term.direction })
-end
-
-for i, term in ipairs(terms) do
-    local key = string.format("<leader>t%d", i)
-
-    vim.keymap.set({ "n" }, key, function()
-        term:toggle(term.size, term.direction)
-    end, { desc = string.format("Toggle Term %d", term.count), noremap = true, silent = true })
-end
-
-vim.keymap.set({ "t" }, "<C-t>", function()
-    for _, term in pairs(terms) do
-        if term:is_open() then
-            term:close()
-        end
-    end
-end, { desc = "Close Terms", noremap = true, silent = true })
 
 vim.keymap.set({ "n" }, "<leader>lg", function()
     local term = Terminal:new({ cmd = "lazygit", direction = "float", size = 0.8 })
     term:toggle()
 end)
-
-vim.keymap.set({ "n" }, "<leader>k9", function()
-    local term = Terminal:new({ cmd = "k9s", direction = "float", size = 0.8 })
-    term:toggle()
-end)
-
-map('t', '<C-w>', '<C-\\><C-n><C-w>')
